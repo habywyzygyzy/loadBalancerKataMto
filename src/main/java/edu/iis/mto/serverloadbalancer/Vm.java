a0dc2e6983b4651468753bd9d778c8744c769299
Kamil Błażejczuk <202119@edu.p.lodz.pl>
2018-06-16 12:31:22 +0200

    balancing zero vms implemented

--- src/main/java/edu/iis/mto/serverloadbalancer/Vm.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Vm.java
@@ -2,10 +2,4 @@ package edu.iis.mto.serverloadbalancer;
 
 public class Vm {
 
-	public int size;
-
-	public Vm(int size) {
-		this.size = size;
-	}
-
 }
--- src/test/java/edu/iis/mto/serverloadbalancer/ServerBuilder.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerBuilder.java
@@ -1,9 +1,8 @@
 package edu.iis.mto.serverloadbalancer;
 
-public class ServerBuilder implements Builder<Server> {
+public class ServerBuilder {
 
 	private int capacity;
-	private double initialLoad;
 
 	public ServerBuilder withCapacity(int capacity) {
 		this.capacity = capacity;
@@ -11,25 +10,7 @@ public class ServerBuilder implements Builder<Server> {
 	}
 
 	public Server build() {
-		Server server = new Server(capacity);
-		addInitialLoad(server);
-		return server;
+		return new Server();
 	}
 
-	private void addInitialLoad(Server server) {
-		if (initialLoad > 0) {
-			int expectedLoad = (int) (initialLoad / Server.MAXIMUM_LOAD * (double) server
-					.getCapacity());
-			server.addVm(VmBuilder.vm().ofSize(expectedLoad).build());
-		}
-	}
-
-	public static ServerBuilder server() {
-		return new ServerBuilder();
-	}
-
-	public ServerBuilder withCurrentLoadOf(double initialLoad) {
-		this.initialLoad = initialLoad;
-		return this;
-	}
 }
