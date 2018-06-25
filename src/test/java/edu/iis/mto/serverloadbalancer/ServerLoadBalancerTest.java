775b57b00adde0feae35e220fc18f31e29ce8328
Kamil Błażejczuk <202119@edu.p.lodz.pl>
2018-06-25 13:47:36 +0200

    balancingOneServerWithTenSlotsCapacity_addOneSlotVm_fillsTheServerWithTenPercent

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -35,6 +35,17 @@ public class ServerLoadBalancerTest {
 		assertThat("server should contain the vm", theServer.contains(theVm));
 	}
 
+	@Test
+	public void balancingOneServerWithTenSlotsCapacity_addOneSlotVm_fillsTheServerWithTenPercent() {
+		Server theServer = a(server().withCapacity(10));
+		Vm theVm = a(vm().ofSize(1));
+
+		balancing(aServerListWith(theServer), aVmsListWith(theVm));
+
+		assertThat(theServer, hasCurrentLoadOf(10.0d));
+		assertThat("server should contain the vm", theServer.contains(theVm));
+	}
+
 	private Vm[] aVmsListWith(Vm... vms) {
 		return vms;
 	}
