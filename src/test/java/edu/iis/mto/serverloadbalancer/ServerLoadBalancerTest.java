43e080a0756dd6ba34377182ae914de729566426
Kamil Błażejczuk <202119@edu.p.lodz.pl>
2018-06-25 15:42:19 +0200

    vmsShouldBeBalancedOnLessLoadedServerFirst

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -60,6 +60,18 @@ public class ServerLoadBalancerTest {
 		assertThat("server should contain the second vm", theServer.contains(theSecondVm));
 	}
 
+	@Test
+	public void vmsShouldBeBalancedOnLessLoadedServerFirst() {
+		Server moreLoadedServer = a(server().withCapacity(100).withCurrentLoadOf(50.0d));
+		Server lessLoadedServer = a(server().withCapacity(100).withCurrentLoadOf(45.0d));
+		Vm theVm = a(vm().ofSize(10));
+
+		balancing(aServerListWith(moreLoadedServer, lessLoadedServer), aVmsListWith(theVm));
+
+		assertThat("more loaded server should contain the vm", lessLoadedServer.contains(theVm));
+		assertThat("less loaded server should not contain the second vm", !moreLoadedServer.contains(theVm));
+	}
+
 	private Vm[] aVmsListWith(Vm... vms) {
 		return vms;
 	}
